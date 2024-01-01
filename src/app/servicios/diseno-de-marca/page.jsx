8dc836a6c50c1984e7b8b7202a13a882747d81de
RJ Luengas <robertojla8@gmail.com>
2023-12-31 20:05:37 -0600

    Se creo la pestaña de "Diseño de marca" y su scss. Además se agregaron las imágenes de la pestaña. Se quitaron los alt que no correspondían a una pestaña

--- src/app/servicios/diseno-de-marca/page.jsx
+++ src/app/servicios/diseno-de-marca/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useRef } from "react";
 
-import "../../../assets/styles/css/services/campaigns/campaigns.css";
+import "../../../assets/styles/css/services/brandDesign/brand_design.css";
 
 //Material UI:
 import { Box, Container, Typography } from "@mui/material";
@@ -12,69 +12,40 @@ import ServiceSectionPackage from "@/assets/components/services/ServiceSectionPa
 import ServiceSectionOurWork from "@/assets/components/services/ServiceSectionOurWork";
 
 //Images:
-import imgCarousel1 from "../../../assets/images/services/campaigns/carousel/1.png";
-import imgCarousel2 from "../../../assets/images/services/campaigns/carousel/2.png";
-import imgCarousel3 from "../../../assets/images/services/campaigns/carousel/3.png";
-import imgCarousel4 from "../../../assets/images/services/campaigns/carousel/4.png";
-import imgCarousel5 from "../../../assets/images/services/campaigns/carousel/5.png";
-import imgCarousel6 from "../../../assets/images/services/campaigns/carousel/6.png";
-import imgCarousel7 from "../../../assets/images/services/campaigns/carousel/7.png";
-import imgCarousel8 from "../../../assets/images/services/campaigns/carousel/8.png";
+import imgCarousel1 from "../../../assets/images/services/brandDesign/carousel/1.jpg";
+import imgCarousel2 from "../../../assets/images/services/brandDesign/carousel/2.jpg";
+import imgCarousel3 from "../../../assets/images/services/brandDesign/carousel/3.jpg";
+import imgCarousel4 from "../../../assets/images/services/brandDesign/carousel/4.jpg";
+import imgCarousel5 from "../../../assets/images/services/brandDesign/carousel/5.jpg";
+import imgCarousel6 from "../../../assets/images/services/brandDesign/carousel/6.jpg";
+import imgCarousel7 from "../../../assets/images/services/brandDesign/carousel/7.jpg";
 
-function Campaigns() {
+function BrandDesign() {
   const containerSectionPackage = useRef(null);
 
   const dataSectionWelcome = {
     title: (
       <>
-        HAZ QUE TE{" "}
-        <Typography sx={{ color: "#7E34C9" }} variant="span">
-          ENCUENTREN
+        CREAMOS MARCAS{" "}
+        <Typography sx={{ color: "#B45201" }} variant="span">
+          EXTRAORDINARIAS
         </Typography>{" "}
       </>
     ),
-    description:
-      "Aumenta las probabilidades de concretar tus ventas a través de Ads.",
+    description: "Creamos una marca única e irrepetible.",
   };
 
   const dataSectionPackage = {
-    title: "GOOGLE Y FACEBOOK ADS",
-    colorCard: "#7E34C9",
+    title: "Diseño de marca",
+    colorCard: "#B45201",
     dataPackage: [
       {
-        title: "Google Ads Display $1,599 MXN",
+        title: "Diseño de marca $5,999 MXN",
         description: null,
         include: [
           "Análisis de mercado.",
           "Selección de palabras clave.",
           "Creación de un anuncio.",
-          "2 imágenes para el anuncio.",
-          "Reporte de resultados.",
-        ],
-        restrictions: null,
-        messageRestrictions: true,
-      },
-      {
-        title: "Google Search $649 MXN",
-        description: null,
-        include: [
-          "Análisis de mercado.",
-          "Selección de palabras clave.",
-          "Creación de un anuncio.",
-          "Reporte de resultados.",
-        ],
-        restrictions: null,
-        messageRestrictions: true,
-      },
-      {
-        title: "Facebook Ads $649 MXN",
-        description: null,
-        include: [
-          "Creación de público especializado.",
-          "Propuesta de 2 diseños.",
-          "Copy in y copy out.",
-          "Recomendación de inversión.",
-          "Monitoreo del periodo de tiempo establecido.",
           "Reporte de resultados.",
         ],
         restrictions: null,
@@ -84,7 +55,7 @@ function Campaigns() {
   };
 
   const dataSectionOurWork = {
-    title: "PROYECTOS DE CAMPAÑA",
+    title: "Nuestros proyectos",
     dataSwiper: [
       {
         id: 0,
@@ -121,11 +92,6 @@ function Campaigns() {
         image: imgCarousel7,
         alt: "",
       },
-      {
-        id: 7,
-        image: imgCarousel8,
-        alt: "",
-      },
     ],
   };
 
@@ -147,7 +113,7 @@ function Campaigns() {
   };
 
   return (
-    <Container maxWidth={false} className="container-service-campaigns">
+    <Container maxWidth={false} className="container-service-brand-design">
       <ServiceSectionWelcome
         dataSectionWelcome={dataSectionWelcome}
         onButtonClick={handleButtonClick}
@@ -157,11 +123,9 @@ function Campaigns() {
         <ServiceSectionPackage dataSectionPackage={dataSectionPackage} />
       </Box>
 
-      <ServiceSectionOurWork
-        dataSectionOurWork={dataSectionOurWork}
-      />
+      <ServiceSectionOurWork dataSectionOurWork={dataSectionOurWork} />
     </Container>
   );
 }
 
-export default Campaigns;
+export default BrandDesign;

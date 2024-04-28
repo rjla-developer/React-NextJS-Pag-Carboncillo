ab55dc07eeba27ebf4ee8d3f0145b2db74a3eb40
RJ Luengas <robertojla8@gmail.com>
2024-04-28 11:13:00 -0600

    Se cambio la tipografía general y de algunos elementos en concreto. Se subieron las tipografías y se separaron los fonts en su propio scss

--- src/assets/components/global/ThemeProviderMUI.jsx
+++ src/assets/components/global/ThemeProviderMUI.jsx
@@ -1,11 +1,16 @@
 "use client"
 import React from "react";
 
+import "../../styles/css/fonts.css";
+
 //Material UI
 import { ThemeProvider, createTheme } from "@mui/material";
 
 function ThemeProviderMUI({children}) {
   const theme = createTheme({
+    typography: {
+      fontFamily: "Helvetica",
+  },
     palette: {
         primary: {
           main: '#56BDCA',
--- src/assets/components/home/HomeSectionResources.jsx
+++ src/assets/components/home/HomeSectionResources.jsx
@@ -7,145 +7,150 @@ import { Box, Button, Container, Grid, Typography } from "@mui/material";
 import Link from "next/link";
 
 function HomeSectionResources() {
-  const dataItemServices = [
-    {
-      id: "social-media",
-      titleService: "Social media",
-      descService:
-        "Establece vínculos emocionales con tus clientes, adéntrate al nuevo mundo de la publicidad digital y mejora tu presencia de marca en la web.",
-      backgroundColorItem: "background-yellow",
-      link: "social-media",
-    },
-    {
-      id: "social-media-superior",
-      titleService: "Redes premium",
-      descService:
-        "Si lo tuyo es una experiencia totalmente personalizada, este plan es perfecto para ti, genera experiencias únicas en tu audiencia.",
-      backgroundColorItem: "background-green",
-      link: "social-media-superior",
-    },
-    {
-      id: "diseno-de-marca",
-      titleService: "Diseño de marca",
-      descService:
-        "Dale una identidad definida y personalizada a tu marca, distingue y destaca tus cualidades empresariales por encima de la competencia.",
-      backgroundColorItem: "background-purple",
-      link: "diseno-de-marca",
-    },
-    {
-      id: "sitios-web",
-      titleService: "Sitios web",
-      descService:
-        "Construye tu espacio digital en internet y haz conocer tu marca con el mundo.",
-      backgroundColorItem: "background-orange",
-      link: "sitios-web",
-    },
-    {
-      id: "campanas-google",
-      titleService: "Google Ads",
-      descService:
-        "Es hora de llevar tu negocio al siguiente nivel, conecta con tu audiencia mediante diversos tipos de publicidad en el buscador más utilizado.",
-      backgroundColorItem: "background-blue",
-      link: "campanas",
-    },
-    {
-      id: "campanas-facebook",
-      titleService: "Facebook Ads",
-      descService:
-        "Un grupo de profesionales listos para ayudarte a conectar con tu audiencia.",
-      backgroundColorItem: "background-rose",
-      link: "campanas",
-    },
-  ];
-  return (
-    <Container>
-      <Grid
-        container
-        spacing={{ xs: 0, md: 5 }}
-        rowSpacing={{ xs: 3, md: 5 }}
-        py={{ xs: 5, md: 10 }}
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Grid item xs={12}>
-          <Typography
-            className="font-family-jaapokkisubtract"
-            sx={{
-              textTransform: "uppercase",
-              color: "white",
-              textAlign: "center",
-              fontSize: { xs: 30, md: 50 },
-              letterSpacing: 8,
-            }}
-          >
-            Recursos
-          </Typography>
-        </Grid>
-
-        {dataItemServices.map((item) => (
-          <Grid
-            key={item.id}
-            item
-            xs={12}
-            md={4}
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Box
-              sx={{
-                backgroundColor: "#292828",
-                borderRadius: "20px",
-                color: "white",
-                padding: 4,
-                height: "250px",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <Box>
-                <Typography fontSize={22} textAlign={"center"}>
-                  {item.titleService}
-                </Typography>
-                <Typography textAlign={"center"} mt={3} fontSize={18}>
-                  {item.descService}
-                </Typography>
-                <Box
-                  width={"100%"}
-                  sx={{
+    const dataItemServices = [
+        {
+            id: "social-media",
+            titleService: "Social media",
+            descService:
+                "Establece vínculos emocionales con tus clientes, adéntrate al nuevo mundo de la publicidad digital y mejora tu presencia de marca en la web.",
+            backgroundColorItem: "background-yellow",
+            link: "social-media",
+        },
+        {
+            id: "social-media-superior",
+            titleService: "Redes premium",
+            descService:
+                "Si lo tuyo es una experiencia totalmente personalizada, este plan es perfecto para ti, genera experiencias únicas en tu audiencia.",
+            backgroundColorItem: "background-green",
+            link: "social-media-superior",
+        },
+        {
+            id: "diseno-de-marca",
+            titleService: "Diseño de marca",
+            descService:
+                "Dale una identidad definida y personalizada a tu marca, distingue y destaca tus cualidades empresariales por encima de la competencia.",
+            backgroundColorItem: "background-purple",
+            link: "diseno-de-marca",
+        },
+        {
+            id: "sitios-web",
+            titleService: "Sitios web",
+            descService:
+                "Construye tu espacio digital en internet y haz conocer tu marca con el mundo.",
+            backgroundColorItem: "background-orange",
+            link: "sitios-web",
+        },
+        {
+            id: "campanas-google",
+            titleService: "Google Ads",
+            descService:
+                "Es hora de llevar tu negocio al siguiente nivel, conecta con tu audiencia mediante diversos tipos de publicidad en el buscador más utilizado.",
+            backgroundColorItem: "background-blue",
+            link: "campanas",
+        },
+        {
+            id: "campanas-facebook",
+            titleService: "Facebook Ads",
+            descService:
+                "Un grupo de profesionales listos para ayudarte a conectar con tu audiencia.",
+            backgroundColorItem: "background-rose",
+            link: "campanas",
+        },
+    ];
+    return (
+        <Container>
+            <Grid
+                container
+                spacing={{ xs: 0, md: 5 }}
+                rowSpacing={{ xs: 3, md: 5 }}
+                py={{ xs: 5, md: 10 }}
+                sx={{
                     display: "flex",
                     justifyContent: "center",
                     alignItems: "center",
-                    mt: 4,
-                  }}
-                >
-                  <Link href={`/servicios/${item.link}`}>
-                    <Button
-                      variant="contained"
-                      sx={{
-                        backgroundColor: "black",
-                        "&:hover": {
-                          backgroundColor: "black",
-                        },
-                      }}
+                }}
+            >
+                <Grid item xs={12}>
+                    <Typography
+                        className="font-family-jaapokkisubtract"
+                        sx={{
+                            textTransform: "uppercase",
+                            color: "white",
+                            textAlign: "center",
+                            fontSize: { xs: 30, md: 50 },
+                            letterSpacing: 8,
+                        }}
                     >
-                      Conoce más
-                    </Button>
-                  </Link>
-                </Box>
-              </Box>
-            </Box>
-          </Grid>
-        ))}
-      </Grid>
-    </Container>
-  );
+                        Recursos
+                    </Typography>
+                </Grid>
+
+                {dataItemServices.map((item) => (
+                    <Grid
+                        key={item.id}
+                        item
+                        xs={12}
+                        md={4}
+                        sx={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                        }}
+                    >
+                        <Box
+                            sx={{
+                                backgroundColor: "#292828",
+                                borderRadius: "20px",
+                                color: "white",
+                                padding: 4,
+                                height: "250px",
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                            }}
+                        >
+                            <Box>
+                                <Typography fontSize={22} textAlign={"center"}>
+                                    {item.titleService}
+                                </Typography>
+                                <Typography
+                                    textAlign={"center"}
+                                    mt={3}
+                                    fontSize={18}
+                                    className="font-family-montserrat"
+                                >
+                                    {item.descService}
+                                </Typography>
+                                <Box
+                                    width={"100%"}
+                                    sx={{
+                                        display: "flex",
+                                        justifyContent: "center",
+                                        alignItems: "center",
+                                        mt: 4,
+                                    }}
+                                >
+                                    <Link href={`/servicios/${item.link}`}>
+                                        <Button
+                                            variant="contained"
+                                            sx={{
+                                                backgroundColor: "black",
+                                                "&:hover": {
+                                                    backgroundColor: "black",
+                                                },
+                                            }}
+                                        >
+                                            Conoce más
+                                        </Button>
+                                    </Link>
+                                </Box>
+                            </Box>
+                        </Box>
+                    </Grid>
+                ))}
+            </Grid>
+        </Container>
+    );
 }
 
 export default HomeSectionResources;

0e2b4aab626248d93e98b05bff0a4ed9f3b629c4
RJ Luengas <robertojla8@gmail.com>
2024-04-28 15:06:42 -0600

    Se subieron las imagenes para los carruseles para laptop y smartphone de la pantalla de website Se igual manera se hicieron avances en esa pantalla, desde su scss hasta avances en el componente Se instalo el paquete de swiper para los carruseles

--- src/app/servicios/sitios-web/page.jsx
+++ src/app/servicios/sitios-web/page.jsx
@@ -4,172 +4,108 @@ import React, { useRef } from "react";
 import "@/assets/styles/css/components/services/websites/websites.css";
 
 //Material UI:
-import { Box, Container, Grid, Typography } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 
 //Components:
 import ServiceSectionWelcome from "@/assets/components/services/ServiceSectionWelcome";
 import ServiceSectionPackage from "@/assets/components/services/ServiceSectionPackage";
-import ServiceSectionOurWork from "@/assets/components/services/ServiceSectionOurWork";
-
-//Images:
-import imgCarousel1 from "@/assets/images/components/services/websites/carousel/1.png";
-import imgCarousel2 from "@/assets/images/components/services/websites/carousel/2.png";
-import imgCarousel3 from "@/assets/images/components/services/websites/carousel/3.webp";
-import imgCarousel4 from "@/assets/images/components/services/websites/carousel/4.png";
-import imgCarousel5 from "@/assets/images/components/services/websites/carousel/5.webp";
-import imgCarousel6 from "@/assets/images/components/services/websites/carousel/6.png";
+import WebsiteSectionCarousels from "@/assets/components/services/websites/website-section-carousels/WebsiteSectionCarousels";
 
 function Website() {
-  const containerSectionPackage = useRef(null);
+    const containerSectionPackage = useRef(null);
 
-  const dataSectionWelcome = {
-    title: (
-      <>
-        WEBSITE SÓLIDOS A LA{" "}
-        <Typography sx={{ color: "#c89d85" }} variant="span">
-        MEDIDA
-        </Typography>{" "}
-      </>
-    ),
-    description: "Creamos una marca única e irrepetible.",
-  };
+    const dataSectionWelcome = {
+        title: (
+            <>
+                WEBSITE SÓLIDOS A LA{" "}
+                <Typography sx={{ color: "#c89d85" }} variant="span">
+                    MEDIDA
+                </Typography>{" "}
+            </>
+        ),
+        description: "Creamos una marca única e irrepetible.",
+    };
 
-  const dataSectionPackage = {
-    title: "PLANES WEB",
-    colorCard: "#c89d85",
-    dataPackage: [
-      {
-        title: "Landing Page $4,999 MXN",
-        description:
-          "Página web diseñada específicamente, para convertir visitantes en leads.",
-        include: [
-          "Página web diseñada específicamente, para convertir visitantes en leads.",
+    const dataSectionPackage = {
+        title: "PLANES WEB",
+        colorCard: "#c89d85",
+        dataPackage: [
+            {
+                title: "Landing Page $4,999 MXN",
+                description:
+                    "Página web diseñada específicamente, para convertir visitantes en leads.",
+                include: [
+                    "Página web diseñada específicamente, para convertir visitantes en leads.",
+                ],
+                restrictions: null,
+                messageRestrictions: true,
+            },
+            {
+                title: "Informativo $8,999 MXN",
+                description:
+                    "Ideal para convertir y proporcionar la esencia informativa de tu marca.",
+                include: [
+                    "Página web diseñada específicamente, para convertir visitantes en leads.",
+                ],
+                restrictions: null,
+                messageRestrictions: true,
+            },
+            {
+                title: "Silver $14,999 MXN",
+                description:
+                    "La herramienta perfecta, para llevar tus servicios o productos a las manos de los consumidores.",
+                include: [
+                    "Página web diseñada específicamente, para convertir visitantes en leads.",
+                ],
+                restrictions: null,
+                messageRestrictions: true,
+            },
+            {
+                title: "Golden $17,999 MXN",
+                description:
+                    "La más completa. Abierto al público las 24 hrs. Sin limitaciones geográficas. Gestión sencilla y rápida. Reduce gastos.",
+                include: [
+                    "Página web diseñada específicamente, para convertir visitantes en leads.",
+                ],
+                restrictions: null,
+                messageRestrictions: true,
+            },
         ],
-        restrictions: null,
-        messageRestrictions: true,
-      },
-      {
-        title: "Informativo $8,999 MXN",
-        description:
-          "Ideal para convertir y proporcionar la esencia informativa de tu marca.",
-        include: [
-          'Página web diseñada específicamente, para convertir visitantes en leads.'
-        ],
-        restrictions: null,
-        messageRestrictions: true,
-      },
-      {
-        title: "Silver $14,999 MXN",
-        description:
-          "La herramienta perfecta, para llevar tus servicios o productos a las manos de los consumidores.",
-        include: [
-          "Página web diseñada específicamente, para convertir visitantes en leads."
-        ],
-        restrictions: null,
-        messageRestrictions: true,
-      },
-      {
-        title: "Golden $17,999 MXN",
-        description:
-          "La más completa. Abierto al público las 24 hrs. Sin limitaciones geográficas. Gestión sencilla y rápida. Reduce gastos.",
-        include: [
-          "Página web diseñada específicamente, para convertir visitantes en leads."
-        ],
-        restrictions: null,
-        messageRestrictions: true,
-      },
-    ],
-  };
+    };
 
-  const dataSectionOurWork = {
-    title: "Nuestros proyectos sitio web",
-    dataSwiper: [
-      {
-        id: 0,
-        image: imgCarousel1,
-        alt: "",
-      },
-      {
-        id: 1,
-        image: imgCarousel2,
-        alt: "",
-      },
-      {
-        id: 2,
-        image: imgCarousel3,
-        alt: "",
-      },
-      {
-        id: 3,
-        image: imgCarousel4,
-        alt: "",
-      },
-      {
-        id: 4,
-        image: imgCarousel5,
-        alt: "",
-      },
-      {
-        id: 5,
-        image: imgCarousel6,
-        alt: "",
-      },
-    ],
-  };
+    const handleButtonClick = () => {
+        containerSectionPackage.current.scrollIntoView({
+            behavior: "smooth",
+            block: "center",
+            inline: "center",
+        });
 
-  const handleButtonClick = () => {
-    containerSectionPackage.current.scrollIntoView({
-      behavior: "smooth",
-      block: "center",
-      inline: "center",
-    });
+        const currentScrollTop = window.scrollY;
+        const containerTop =
+            containerSectionPackage.current.getBoundingClientRect().top +
+            currentScrollTop;
+        window.scrollTo({
+            top: containerTop - 50,
+            behavior: "smooth",
+        });
+    };
 
-    const currentScrollTop = window.scrollY;
-    const containerTop =
-      containerSectionPackage.current.getBoundingClientRect().top +
-      currentScrollTop;
-    window.scrollTo({
-      top: containerTop - 50,
-      behavior: "smooth",
-    });
-  };
+    return (
+        <Container maxWidth={false} className="container-service-website">
+            <ServiceSectionWelcome
+                dataSectionWelcome={dataSectionWelcome}
+                onButtonClick={handleButtonClick}
+            />
 
-  return (
-    <Container maxWidth={false} className="container-service-website">
-      <ServiceSectionWelcome
-        dataSectionWelcome={dataSectionWelcome}
-        onButtonClick={handleButtonClick}
-      />
+            <Box ref={containerSectionPackage}>
+                <ServiceSectionPackage
+                    dataSectionPackage={dataSectionPackage}
+                />
+            </Box>
 
-      <Box ref={containerSectionPackage}>
-        <ServiceSectionPackage dataSectionPackage={dataSectionPackage} />
-      </Box>
-
-      {/* <ServiceSectionOurWork dataSectionOurWork={dataSectionOurWork} /> */}
-      <Container sx={{ pt: { xs: 0, md: 10 }, pb: { xs: 8, md: 10 } }}>
-            <Grid container sx={{ color: "white" }}>
-                <Grid item xs={12}>
-                    <Box>
-                        <Typography
-                            sx={{
-                                textTransform: "uppercase",
-                                textAlign: "center",
-                                fontSize: { xs: 25, lg: 40 },
-                                fontWeight: "900",
-                            }}
-                        >
-                            {dataSectionOurWork.title}
-                        </Typography>
-                        <Typography sx={{ textAlign: "center", mt: 2 }}>
-                            ¡Da un vistazo a nuestro trabajo!
-                        </Typography>
-
-                    </Box>
-                </Grid>
-            </Grid>
+            <WebsiteSectionCarousels />
         </Container>
-    </Container>
-  );
+    );
 }
 
 export default Website;
---
+++ src/assets/components/services/websites/website-section-carousels/WebsiteSectionCarousels.jsx
@@ -0,0 +1,125 @@
+import React from "react";
+
+//CSS:
+import "@/assets/styles/css/components/services/websites/website-section-carousels/website-section-carousels.css";
+
+//Material UI:
+import { Box, Container, Grid, Typography } from "@mui/material";
+
+//Next:
+import Image from "next/image";
+
+//Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+
+//Import Swiper styles
+import "swiper/css";
+
+//Images:
+import imgLaptopMockup from "@/assets/images/components/services/websites/laptop/laptop-mockup.png";
+import imgSmartphoneMockup from "@/assets/images/components/services/websites/smartphone/smartphone-mockup.png";
+import imgWebsiteAerolineaLaptop from "@/assets/images/components/services/websites/laptop/carousel-laptop/aerolinea-laptop.png";
+import imgWebsiteAerolineaMobile from "@/assets/images/components/services/websites/smartphone/carousel-smartphone/aerolinea-smartphone.png";
+import imgWebsiteElectronicaLaptop from "@/assets/images/components/services/websites/laptop/carousel-laptop/electronica-laptop.png";
+import imgWebsiteElectronicaMobile from "@/assets/images/components/services/websites/smartphone/carousel-smartphone/electronica-smartphone.png";
+import imgWebsiteJuegosLaptop from "@/assets/images/components/services/websites/laptop/carousel-laptop/juegos-vortice-laptop.png";
+import imgWebsiteJuegosMobile from "@/assets/images/components/services/websites/smartphone/carousel-smartphone/juegos-vortice-smartphone.png";
+
+function WebsiteSectionCarousels() {
+    const dataSectionOurWork = {
+        title: "Nuestros proyectos sitio web",
+        dataLaptopCarousel: [
+            {
+                id: 0,
+                image: imgWebsiteAerolineaLaptop,
+                alt: "Diseño de una página web de una aerolinea",
+            },
+            {
+                id: 1,
+                image: imgWebsiteElectronicaLaptop,
+                alt: "Diseño de una página web de una tienda de electrónica",
+            },
+            {
+                id: 2,
+                image: imgWebsiteJuegosLaptop,
+                alt: "Diseño de una página web de una tienda de videojuegos",
+            },
+        ],
+        dataSmartphoneCarousel: [
+            {
+                id: 0,
+                image: imgWebsiteAerolineaMobile,
+                alt: "Diseño smartphone de una página web de una aerolinea",
+            },
+            {
+                id: 1,
+                image: imgWebsiteElectronicaMobile,
+                alt: "Diseño smartphone de una página web de una tienda de electrónica",
+            },
+            {
+                id: 2,
+                image: imgWebsiteJuegosMobile,
+                alt: "Diseño smartphone de una página web de una tienda de videojuegos",
+            },
+        ],
+    };
+    return (
+        <Container
+            sx={{
+                pt: { xs: 0, md: 10 },
+                pb: { xs: 8, md: 10 },
+                /* backgroundColor: "green", */
+            }}
+        >
+            <Box sx={{ color: "white" }}>
+                <Typography
+                    sx={{
+                        textTransform: "uppercase",
+                        textAlign: "center",
+                        fontSize: { xs: 25, lg: 40 },
+                        fontWeight: "900",
+                    }}
+                >
+                    {dataSectionOurWork.title}
+                </Typography>
+                <Typography sx={{ textAlign: "center", mt: 2 }}>
+                    ¡Da un vistazo a nuestro trabajo!
+                </Typography>
+            </Box>
+            <Grid container sx={{ mt: 8 }}>
+                <Grid item xs={12} md={6}>
+                    <Box>
+                        <Image
+                            src={imgLaptopMockup}
+                            alt="Laptop mockup"
+                            className="img-laptop-mockup"
+                        />
+                        <Swiper
+                            className="mySwiper"
+                            loop={true}
+                            autoplay={{
+                                delay: 4500,
+                                disableOnInteraction: false,
+                            }}
+                            modules={[Autoplay]}
+                        >
+                            {dataSectionOurWork.dataLaptopCarousel.map(
+                                (item) => (
+                                    <SwiperSlide key={item.id}>
+                                        <Image
+                                            src={item.image}
+                                            alt={item.alt}
+                                        />
+                                    </SwiperSlide>
+                                )
+                            )}
+                        </Swiper>
+                    </Box>
+                </Grid>
+            </Grid>
+        </Container>
+    );
+}
+
+export default WebsiteSectionCarousels;

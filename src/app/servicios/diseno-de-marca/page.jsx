621cc7d9397926d161bfd37bd08c106a8b667bd9
RJ Luengas <robertojla8@gmail.com>
2024-04-29 07:46:36 -0600

    Se cambio la información que va en el diseño de marca

--- src/app/servicios/diseno-de-marca/page.jsx
+++ src/app/servicios/diseno-de-marca/page.jsx
@@ -25,131 +25,130 @@ import imgCarousel10 from "@/assets/images/components/services/brandDesign/carou
 import imgCarousel11 from "@/assets/images/components/services/brandDesign/carousel/11.png";
 
 function BrandDesign() {
-  const containerSectionPackage = useRef(null);
+    const containerSectionPackage = useRef(null);
 
-  const dataSectionWelcome = {
-    title: (
-      <>
-        CREAMOS MARCAS{" "}
-        <Typography sx={{ color: "#B45201" }} variant="span">
-          EXTRAORDINARIAS
-        </Typography>{" "}
-      </>
-    ),
-    description: "Creamos una marca única e irrepetible.",
-  };
+    const dataSectionWelcome = {
+        title: (
+            <>
+                CREAMOS MARCAS{" "}
+                <Typography sx={{ color: "#B45201" }} variant="span">
+                    EXTRAORDINARIAS
+                </Typography>{" "}
+            </>
+        ),
+        description: "Creamos una marca única e irrepetible.",
+    };
 
-  const dataSectionPackage = {
-    title: "Diseño de marca",
-    colorCard: "#B45201",
-    dataPackage: [
-      {
-        title: "Diseño de marca $5,999 MXN",
-        description: null,
-        include: [
-          "Análisis de mercado.",
-          "Selección de palabras clave.",
-          "Creación de un anuncio.",
-          "Reporte de resultados.",
+    const dataSectionPackage = {
+        title: "Diseño de marca",
+        description:
+            "¿Estás listo para que tu marca deje una huella indeleble en el mercado?",
+        colorCard: "#B45201",
+        dataPackage: [
+            {
+                title: "Diseño de marca $4,999 MXN",
+                description: "Nuestro enfoque holístico incluye desde la creación de un logotipo y sus variaciones, pasando por la selección meticulosa de colores y tipografías, hasta la definición de un estilo gráfico único que elevará tu presencia en cualquier medio. ",
+                include: null,
+                restrictions: null,
+                messageRestrictions: true,
+            },
         ],
-        restrictions: null,
-        messageRestrictions: true,
-      },
-    ],
-  };
+    };
 
-  const dataSectionOurWork = {
-    title: "Nuestros proyectos",
-    dataSwiper: [
-      {
-        id: 0,
-        image: imgCarousel1,
-        alt: "",
-      },
-      {
-        id: 1,
-        image: imgCarousel2,
-        alt: "",
-      },
-      {
-        id: 2,
-        image: imgCarousel3,
-        alt: "",
-      },
-      {
-        id: 3,
-        image: imgCarousel4,
-        alt: "",
-      },
-      {
-        id: 4,
-        image: imgCarousel5,
-        alt: "",
-      },
-      {
-        id: 5,
-        image: imgCarousel6,
-        alt: "",
-      },
-      {
-        id: 6,
-        image: imgCarousel7,
-        alt: "",
-      },
-      {
-        id: 7,
-        image: imgCarousel8,
-        alt: "",
-      },
-      {
-        id: 8,
-        image: imgCarousel9,
-        alt: "",
-      },
-      {
-        id: 9,
-        image: imgCarousel10,
-        alt: "",
-      },
-      {
-        id: 10,
-        image: imgCarousel11,
-        alt: "",
-      },
-    ],
-  };
+    const dataSectionOurWork = {
+        title: "Nuestros proyectos",
+        dataSwiper: [
+            {
+                id: 0,
+                image: imgCarousel1,
+                alt: "",
+            },
+            {
+                id: 1,
+                image: imgCarousel2,
+                alt: "",
+            },
+            {
+                id: 2,
+                image: imgCarousel3,
+                alt: "",
+            },
+            {
+                id: 3,
+                image: imgCarousel4,
+                alt: "",
+            },
+            {
+                id: 4,
+                image: imgCarousel5,
+                alt: "",
+            },
+            {
+                id: 5,
+                image: imgCarousel6,
+                alt: "",
+            },
+            {
+                id: 6,
+                image: imgCarousel7,
+                alt: "",
+            },
+            {
+                id: 7,
+                image: imgCarousel8,
+                alt: "",
+            },
+            {
+                id: 8,
+                image: imgCarousel9,
+                alt: "",
+            },
+            {
+                id: 9,
+                image: imgCarousel10,
+                alt: "",
+            },
+            {
+                id: 10,
+                image: imgCarousel11,
+                alt: "",
+            },
+        ],
+    };
 
-  const handleButtonClick = () => {
-    containerSectionPackage.current.scrollIntoView({
-      behavior: "smooth",
-      block: "center",
-      inline: "center",
-    });
+    const handleButtonClick = () => {
+        containerSectionPackage.current.scrollIntoView({
+            behavior: "smooth",
+            block: "center",
+            inline: "center",
+        });
 
-    const currentScrollTop = window.scrollY;
-    const containerTop =
-      containerSectionPackage.current.getBoundingClientRect().top +
-      currentScrollTop;
-    window.scrollTo({
-      top: containerTop - 50,
-      behavior: "smooth",
-    });
-  };
+        const currentScrollTop = window.scrollY;
+        const containerTop =
+            containerSectionPackage.current.getBoundingClientRect().top +
+            currentScrollTop;
+        window.scrollTo({
+            top: containerTop - 50,
+            behavior: "smooth",
+        });
+    };
 
-  return (
-    <Container maxWidth={false} className="container-service-brand-design">
-      <ServiceSectionWelcome
-        dataSectionWelcome={dataSectionWelcome}
-        onButtonClick={handleButtonClick}
-      />
+    return (
+        <Container maxWidth={false} className="container-service-brand-design">
+            <ServiceSectionWelcome
+                dataSectionWelcome={dataSectionWelcome}
+                onButtonClick={handleButtonClick}
+            />
 
-      <Box ref={containerSectionPackage}>
-        <ServiceSectionPackage dataSectionPackage={dataSectionPackage} />
-      </Box>
+            <Box ref={containerSectionPackage}>
+                <ServiceSectionPackage
+                    dataSectionPackage={dataSectionPackage}
+                />
+            </Box>
 
-      <ServiceSectionOurWork dataSectionOurWork={dataSectionOurWork} />
-    </Container>
-  );
+            <ServiceSectionOurWork dataSectionOurWork={dataSectionOurWork} />
+        </Container>
+    );
 }
 
 export default BrandDesign;
--- src/assets/components/services/ServiceSectionPackage.jsx
+++ src/assets/components/services/ServiceSectionPackage.jsx
@@ -2,55 +2,58 @@ import React from "react";
 
 //Material UI:
 import { Box, Container, Grid, Typography } from "@mui/material";
+
+//Components:
 import CardPackage from "./ComponentsServicesSectionPackage/CardPackage";
 
 function ServiceSectionPackage({ dataSectionPackage }) {
-  return (
-    <Container sx={{ color: "white", py: 5 }} maxWidth={false}>
-      <Grid container>
-        <Grid item xs={12}>
-          <Box>
-            <Typography
-              sx={{
-                textTransform: "uppercase",
-                textAlign: "center",
-                fontSize: { xs: 30, lg: 45 },
-                fontWeight: "900",
-              }}
-            >
-              {dataSectionPackage.title}
-            </Typography>
-            <Typography sx={{ textAlign: "center", mt: 2 }}>
-              Tenemos para ti una gran variedad de soluciones profesionales,
-              para lograr tus objetivos de venta.
-            </Typography>
-          </Box>
-        </Grid>
-        <Grid item xs={12}>
-          <Grid
-            container
-            columnGap={1.5}
-            sx={{
-              mt: 5,
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            {dataSectionPackage.dataPackage.map((dataItem) => {
-              return (
-                <CardPackage
-                  key={dataItem.title}
-                  dataItem={dataItem}
-                  colorCard={dataSectionPackage.colorCard}
-                />
-              );
-            })}
-          </Grid>
-        </Grid>
-      </Grid>
-    </Container>
-  );
+    return (
+        <Container sx={{ color: "white", py: 5 }} maxWidth={false}>
+            <Grid container>
+                <Grid item xs={12}>
+                    <Box>
+                        <Typography
+                            sx={{
+                                textTransform: "uppercase",
+                                textAlign: "center",
+                                fontSize: { xs: 30, lg: 45 },
+                                fontWeight: "900",
+                            }}
+                        >
+                            {dataSectionPackage.title}
+                        </Typography>
+                        <Typography sx={{ textAlign: "center", mt: 2 }}>
+                            {dataSectionPackage.description
+                                ? dataSectionPackage.description
+                                : "Tenemos para ti una gran variedad de soluciones profesionales, para lograr tus objetivos de venta."}
+                        </Typography>
+                    </Box>
+                </Grid>
+                <Grid item xs={12}>
+                    <Grid
+                        container
+                        columnGap={1.5}
+                        sx={{
+                            mt: 5,
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                        }}
+                    >
+                        {dataSectionPackage.dataPackage.map((dataItem) => {
+                            return (
+                                <CardPackage
+                                    key={dataItem.title}
+                                    dataItem={dataItem}
+                                    colorCard={dataSectionPackage.colorCard}
+                                />
+                            );
+                        })}
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Container>
+    );
 }
 
 export default ServiceSectionPackage;
